921436615987efa7c0cbacc2e166b51c27a8be4c
DLamarG <demondg8@gmail.com>
2024-09-04 14:30:29 -0500

    second push

--- backend_api/backend_api/settings.py
+++ backend_api/backend_api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,13 @@ WSGI_APPLICATION = 'backend_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+    "ENGINE": "django.db.backends.postgresql",
+    "NAME": os.getenv("DATABASE_NAME", "recipe_app_db"),
+    "USER": os.getenv("DATABASE_USER", "postgres"),
+    "PASSWORD": os.getenv("DATABASE_PASSWORD", "postgres"),
+    "HOST": "recipe_db",
+    "PORT": 5432,
     }
 }
 
