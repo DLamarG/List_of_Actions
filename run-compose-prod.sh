eee63fa31ec93a3a0c5a1e5ac4b237fcef57fc5e
DLamarG <demondg8@gmail.com>
2024-09-05 16:05:16 -0500

    changed run-compose

--- run-compose-prod.sh
+++ run-compose-prod.sh
@@ -5,5 +5,5 @@ docker-compose -f docker_compose.dev.yml up -d --build
 
 # make sure the postgres container is ready, then run migrations
 sleep 10 
-docker exec list_of_actions-task-api-1  python /src/manage.py makemigrations 
-docker exec list_of_actions-task-api-1  python /src/manage.py migrate
+docker exec list_of_actions-api-1  python /src/manage.py makemigrations 
+docker exec list_of_actions-api-1  python /src/manage.py migrate
